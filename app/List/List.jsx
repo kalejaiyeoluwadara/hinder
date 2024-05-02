dfb96247512105f53f156dce374aa9c50328bb2b
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-05-02 20:52:11 +0100

    updates

--- app/List/List.jsx
+++ app/List/List.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+// 'use client'
 import Single from './Single'
 import Link from 'next/link';
-
+// import data from '../components/data'
+// import { useState } from 'react';
 function List() {
+    // const [lists,setLists] = useState(data)
+    // console.log(lists);
   return (
     <div className='flex flex-col min-h-screen  w-full px-4 my-6 pb-20  items-center justify-center gap-12 ' >
-      {[1,2,3,4,5].map(()=>{
+      {[1,2,3].map(()=>{
         return (
           <Link href={'/view'} >
             <Single />
