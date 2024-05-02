ca6a2a285e19fb83e93592bc9b83645e849a5650
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-05-02 16:10:10 +0100

    first update

---
+++ app/components/nav.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import logo from './dojo-logo.png'
+function Nav() {
+  return (
+   <nav className=''>
+         
+        </nav>
+  )
+}
+
+export default Nav
---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Main</h1>
+      </main>
+  )
+}
