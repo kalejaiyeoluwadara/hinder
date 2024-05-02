a436de285e0d2d1dfd36a2ed84ec3765dedc0564
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-05-02 23:00:50 +0100

    done

--- app/components/data.js
+++ app/components/data.js
@@ -2,7 +2,7 @@ export const data =
 [{
     img:"https://images.unsplash.com/photo-1541089404510-5c9a779841fc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     poster:'singlebae',
-    hates:20,
+    hates:220,
     desc: 'Have you guys seen this annoying couple? always post...',
     comments:[{
         user:'',
@@ -16,7 +16,7 @@ export const data =
 {
     img: "https://images.unsplash.com/photo-1501901609772-df0848060b33?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGNvdXBsZXxlbnwwfHwwfHx8MA%3D%3D",
     poster: 'singlepringle',
-    hates: 20,
+    hates: 80,
     desc: 'Have you guys t...',
     comments: [{
         user: '',
@@ -30,7 +30,7 @@ export const data =
 {
     img: "https://images.unsplash.com/photo-1591969851586-adbbd4accf81?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNvdXBsZXxlbnwwfHwwfHx8MA%3D%3D",
     poster: 'pringlesingle',
-    hates: 20,
+    hates: 125,
     desc: 'Have you guys seen this annoying couple? always post...',
     comments: [{
         user: '',
@@ -44,7 +44,7 @@ export const data =
 {
     img: "https://plus.unsplash.com/premium_photo-1683740128746-176cff24b9a6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGNvdXBsZXxlbnwwfHwwfHx8MA%3D%3D",
     poster: 'rosa',
-    hates: 20,
+    hates: 90,
     desc: 'Have you guys seen this annoying couple? always post...',
     comments: [{
         user: '',
@@ -58,7 +58,7 @@ export const data =
 {
     img: 'https://images.unsplash.com/photo-1541089255538-4ea146c8866d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D',
     poster: 'Single Prince',
-    hates: 20,
+    hates: 76,
     desc: 'Have you guys seen this annoying couple? always post...',
     comments: [{
         user: '',
