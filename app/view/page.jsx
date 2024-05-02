160283f51fc455509d1eb519751b33bfb8251c17
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-05-02 19:49:34 +0100

    moving

--- app/view/page.jsx
+++ app/view/page.jsx
@@ -24,19 +24,51 @@ export default function View() {
           </div>
         </div>
 
-        <div className="flex justify-between mt-3 items-center ">
+        <div className="flex justify-between mt-4 items-start ">
           <div className="flex flex-col">
-            <p>@singlebae's post</p>
-            <p className="text-[11px] opacity-70 ">2 min ago</p>
+            <p className="font-[600]">@singlebae's post</p>
+            <p className="text-[13px] opacity-70 ">2 min ago</p>
           </div>
           <div className="flex items-center text-red-700">
-            <p className=" font-[600] text-[17px]  ">Destroy with Hinder</p>
+            <p className=" font-[600] text-[15px]  ">Destroy with Hinder</p>
             <MdChevronRight size={25} />
           </div>
         </div>
       </section>
-      <section></section>
-      <section></section>
+      {/* Description */}
+      <section className="my-4">
+        <p className="text-[16px] font-[500] ">
+          Have you guys seen this annoying couple? always posting their nonsense
+          self online, we can sha destroy their relationship on hinder.
+        </p>
+      </section>
+      {/* Comments */}
+      <section className="w-full flex flex-col">
+        <div className="flex items-center mb-2 justify-between w-full">
+          <p className="opacity-80 text-[16px] ">Comments</p>
+          <button className="px-4 py-2 bg-red-100 text-[16px] font-[600] text-red-900 rounded-[20px] ">
+            + Add comment
+          </button>
+        </div>
+        {/* comment items */}
+        <div className="flex items-center mt-2 justify-between" >
+          <div className="flex items-center gap-1">
+            <Image
+              src={c1}
+              alt="Logo"
+              className="rounded-[50%] h-[40px] w-[40px] object-cover  "
+              layout="fixed"
+            />
+            <p className="ml-2 opacity-60 font-[500] text-[14px] ">@single2</p>
+          </div>
+          <button
+            className="px-4 py-2 bg-gray-100 text-[16px] font-[600]
+            rounded-[20px] "
+          >
+            Reply
+          </button>
+        </div>
+      </section>
     </main>
   );
 }
